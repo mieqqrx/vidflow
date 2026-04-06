042752575af7a58cc45049a43fc60dd1f3b06542
unknown <misevdmitro@gmail.com>
2026-04-06 19:45:23 +0300

    added new tests

--- tests/vidflow.tests/VideoSubscribeTest.cs
+++ tests/vidflow.tests/VideoSubscribeTest.cs
@@ -9,14 +9,14 @@ namespace vidflow.tests
     {
         private readonly IWebDriver _driver;
         private readonly WebDriverWait _wait;
-        private const string url= "http://localhost:3000";
+        private const string url = "http://localhost:3000";
 
         public VideoSubscribeTest()
         {
-           
+
             _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
-            _wait = new WebDriverWait(_driver,TimeSpan.FromSeconds(5));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
         }
         [Fact]
         public void Subscribe_Channel_Test()
@@ -44,10 +44,35 @@ namespace vidflow.tests
             var searchBtn = _driver.FindElement(By.CssSelector("button[type='submit']"));
             searchBtn.Click();
             Thread.Sleep(2200);
-            var subscribeButton = _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[text()='Subscribe']")));
-            subscribeButton.Click();
+            var actionButton = _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[contains(., 'Subscribe') or contains(., 'Subscribed')]")));
+            if(actionButton.Text.Contains("Subscribed"))
+                {
+                actionButton.Click();
+                Thread.Sleep(1000);
+                _wait.Until(d => !actionButton.Text.Contains("Subscribed") && actionButton.Text.Contains("Subscribe"));
+                actionButton.Click();
+            }
+            else
+            {
+                actionButton.Click();
+            }
             Thread.Sleep(1000);
-            _wait.Until(d => d.FindElement(By.XPath("//button[contains(text(), 'Subscribed')]")));
+
+            _wait.Until(d => {
+                try
+                {
+                var subscribedSpan = d.FindElement(By.XPath("//button//span[text()='Subscribed']"));
+                return subscribedSpan.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
             _driver.Navigate().GoToUrl(url);
             var subscribedChannel=_wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[contains(@href, '/channel/')]//span[text()='{channelName}']")));
             Assert.NotNull(subscribedChannel);
